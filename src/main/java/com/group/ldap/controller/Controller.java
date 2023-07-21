cf146969d6faef3c24c2d7cc122fd9e784b942ae
MikeZowa <134649451+MikeZowa@users.noreply.github.com>
2023-07-21 10:50:06 +0200

    controller no security

---
+++ src/main/java/com/group/ldap/controller/Controller.java
@@ -0,0 +1,19 @@
+package com.group.ldap.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+@RequestMapping("/api")
+public class Controller {
+
+	@GetMapping("/")
+	public String showMessageOfTheDay(){
+		return "Hello its: "+ LocalDateTime.now()
+				.format(DateTimeFormatter.ofPattern("HH:mm 'on a' EEEE  'the' dd-MM-yyyy"));
+	}
+}
